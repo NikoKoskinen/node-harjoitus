47f47ad56a30f6fd21592c67f97c57841a94eeed
NikoKoskinen <111054049+NikoKoskinen@users.noreply.github.com>
2023-08-31 12:05:47 +0300

    table muokkaus + hourly loop

--- app.js
+++ app.js
@@ -30,27 +30,41 @@ app.set('view engine', 'handlebars');
 // -----------
 // TODO: add date and time as dynamic data for the homepage, is it sensible to use server for creating time values?
 
+//Route to homepage 
 app.get('/', (req, res) => {
 
+    // handlebars needs a key to show  data on page, jston is a good way to send it
     let homePageData = {
         'price': 31.25,
         'wind': 3,
         'temperature': 20
     }
+
+    // Render index. Handlebars and send dynamic data to the page
     res.render('index', homePageData)
 
 });
 
+// Route to hourly data page
 app.get('/hourly', (req, res) => {
 
-    let hourlyPageData = {
-        'hour': 13,
-        'price': 55.65
-    }
+    // Data will be presented on a table. To loop all rows we need a key for table and for column data
+    let hourlyPageData = { 'tableData': [
+        {'hour': 13,
+        'price': 55.65},
+        {'hour': 14,
+        'price': 35.65},,
+        {'hour': 15,
+        'price': 77.65},
+        {'hour': 16,
+        'price': 9.65}
+    ]
+    };
+
     res.render('hourly',hourlyPageData)
 
 });
 
 // START THE LISTENER
 app.listen(PORT)
-console.log('server started and it will listen TCp port', PORT);
+console.log('server started and it will listen TCP port', PORT);
