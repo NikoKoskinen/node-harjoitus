7ecfff85823794e2a196130b5689eaaf84946ac8
NikoKoskinen <111054049+NikoKoskinen@users.noreply.github.com>
2023-09-27 16:07:07 +0300

    Muutokset 26.9

--- scheduleMS.js
+++ scheduleMS.js
@@ -1,71 +1,78 @@
-/*A MICROSERFICE FOR SCHEDULING DATA IMPORTS FROM PORSSISAHKO.NET API
-New data is avaible daily at 12.00 UTC local time or 15.00 when
-daylight saving time or summer time is on */
+/* A MICROSERVICE FOR SCHEDULING DATA IMPORTS FROM PORSSISAHKO.NET API 
+New data is available daily at 12.00 UTC 14.00 localt time or 15.00 when
+daylight saving time or summer time is on. */
 
 // LIBRARIES AND MODULES
 
 const cron = require('node-cron') // The scheduling library
 
-// Run a function every day at 15:30
-cron.schedule('30 15  * * *',() => {
-    console.log('this will be executed daily at 15:30')
+// Run a function every day at 15:30 
+cron.schedule('30 15 * * *', () => {
+    console.log('This will be executed daily at 15:30')
 });
 
-// A test to check scheduler: run it every minute
-cron.schedule('* * * * *',() => {
-    console.log('this will be executed every minute')
+// A test to check sceduler: run it every minute
+cron.schedule('* * * * *', () => {
+    console.log('This will be executed every minute')
 });
 
-// A test to check scheduler: run it every hour
-cron.schedule('0 * * * *',() => {
-    console.log('this will be executed every hour')
+// A test to check sceduler: run it every hour
+cron.schedule('0 * * * *', () => {
+    console.log('This will be executed every hour')
+});
+
+// A test to check sceduler: run it every 5 minutes
+cron.schedule('*/5 * * * *', () => {
+    console.log('This will be executed 5 minutes interval')
 });
 
 // Running the task every full hour according to a list
-cron.schedule('0 15,19,22 * * * *',() => {
-    console.log('this will be executed at 3,7 and 10 PM')
+cron.schedule('0 15,19,22 * * *', () => {
+    console.log("This will be executed at 3, 7 and 10 o'clock PM")
 });
 
-// Running the task hoyrly between 15 and 20
-cron.schedule('0 15-22 * * * *',() => {
-    console.log('this will be executed at every hour from 3 to 8 PM')
-});
-
-// A test to check scheduler: run it every 5 minutes
-cron.schedule('*/5 * * * *',() => {
-    console.log('this will be executed every 5 minutes interval')
+// Runnig the task hourly between 15 and 20
+cron.schedule('0 15-20 * * *', () => {
+    console.log("This will be executed every hour from 3 to 8 o'clock PM")
 });
 
 // Use error handling when running a task
 cron.schedule('30 15 * * *', () => {
-   try { 
-        console.log('this will be executed daily at 15:30')}
+    try {
+        console.log('This will be executed daily at 15:30')}
     catch (error) {
         console.log('An error occurred') }
     });
 
-    // TODO: FIX counter or flag check the logic of error counter
-// Use a counter variable to keep record of successfull operation
-let counter = 0 // Set the initial value
 
-// Check if the counter is to run an operation in 5 minute intervals frpm 3 to 4 PM
-cron.schedule('*/5 15 * * *', () => { // Initialize flag to true
-    try { 
-        // Check if the flag is still true to run the operation
-        if (counter == 0) {
-            console.log('this will be executed every 5 mins at 15:00 until 15:55')};
-            counter =+ 1; // If successfull operation set the counter to the initial value
+// Fetch data and try again in 5 minute intervals if an attempt fails
+
+// Use a date variable to keep track of successfull data retrievals
+let lastFetchedDate = '1.1.2023' // Initial value, in production use settings file
+
+// Try to run an operation in 5 minute intervals from 3 to 4 PM
+cron.schedule('*/5 15 * * *', () => {
+    try {
+        let timestamp = new Date() // Get the current timestamp
+        let dateStr = timestamp.toLocaleDateString() // Take datepart of the timestamp
+
+        // If the date of last sucessfull fetch is not the current day, get data
+        if (lastFetchedDate != dateStr) {
+            console.log('This is an attempt to run scheduled fetch operation');
+            lastFetchedDate = dateStr // Set fetch date to current date
+
         }
-     catch (error) {
-         console.log('An error occurred');
 
-         counter  } 
-         if (counter >= 12) {
-            counter = 0;
-         }
-     });
- 
-
+        else {
+            console.log('Data has been successfully retrieved earlier today')
+        }
+    }
+        
+    catch (error) {
+        console.log('An error occurred, trying again in 5 minutes until 4 PM');
+        
+    }
+});
 
 
 
